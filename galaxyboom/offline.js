8ec7f0130ead5e9a8a08ab44f22b36cbe4823d5e
TheCompClub <51406630+TheCompClub@users.noreply.github.com>
2019-06-14 09:01:01 -0400

    Galaxy Boom

---
+++ galaxyboom/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1487705794,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/nyancat-sheet0.png",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/alienspaceship-sheet0.png",
+		"media/235968__tommccann__explosion-01.m4a",
+		"media/235968__tommccann__explosion-01.ogg",
+		"media/344142__brokenphono__gunshot-002.m4a",
+		"media/344142__brokenphono__gunshot-002.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
