ffcc3c6d55ee98a38094aeab936847b304fba938
TheCompClub <51406630+TheCompClub@users.noreply.github.com>
2019-06-14 08:42:07 -0400

    This Boarding Party is Over

---
+++ boardingparty/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1497443454,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground2.png",
+		"images/enemy-sheet0.png",
+		"images/protect-sheet0.png",
+		"images/player-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/explode-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground3.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"media/the wilhelm scream sound effect.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
